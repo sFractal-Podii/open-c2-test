cf1375af2d121035dba4852420154628a1aeb460
TraceyOnim <traceyonim22@gmail.com>
2023-06-09 15:33:15 +0300

    increase version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpencC2Test.MixProject do
   def project do
     [
       app: :openc_c2_test,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
