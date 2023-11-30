14bf0cf9dc18b7b15ff608b5c9962de597283d48
TraceyOnim <traceyonim22@gmail.com>
2023-11-30 23:16:44 +0300

    add emqtt client to enable publishing

---
+++ lib/openc_c2_test/emqtt/emqtt.ex
@@ -0,0 +1,92 @@
+defmodule Emqtt do
+  @moduledoc "Emqtt server responsible for handling pubsub between clients and broker"
+  use GenServer
+  require Logger
+
+  @clean_start false
+
+  def start_link([]) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init([]) do
+    topic = "oc2/cmd/device/t01"
+
+    clientid =
+      System.get_env("CLIENT_ID") ||
+        raise """
+        environment variable CLIENT_ID is missing.
+        For example:
+        export CLIENT_ID=openc2test2023
+        """
+
+    Logger.info("client_id is #{clientid}")
+
+    host =
+      ~c"#{System.get_env("MQTT_HOST")}" ||
+        raise """
+        environment variable HOST is missing.
+        Examples:
+        export MQTT_HOST="35.221.11.97 "
+        export MQTT_HOST="mqtt.broker.com"
+        """
+
+    Logger.info("mqtt_host is #{host}")
+
+    port =
+      String.to_integer(
+        System.get_env("MQTT_PORT") ||
+          raise("""
+          environment variable MQTT_PORT is missing.
+          Example:
+          export MQTT_PORT=1883
+          """)
+      )
+
+    Logger.info("mqtt_port is #{port}")
+
+    name =
+      String.to_atom(System.get_env("USER_NAME")) ||
+        raise """
+        environment variable USER_NAME is missing.
+        Examples:
+        export USER_NAME="plug"
+        """
+
+    Logger.info("user_name is #{name}")
+
+    emqtt_opts = [
+      host: host,
+      port: port,
+      clientid: clientid,
+      clean_start: @clean_start,
+      name: name
+    ]
+
+    {:ok, pid} = :emqtt.start_link(emqtt_opts)
+
+    state = %{pid: pid, topic: topic}
+
+    {:ok, state, {:continue, :start_emqtt}}
+  end
+
+  def handle_continue(:start_emqtt, %{pid: pid} = state) do
+    {:ok, _} = :emqtt.connect(pid)
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:publish, message}, %{topic: topic, pid: pid} = state) do
+    :emqtt.publish(
+      pid,
+      topic,
+      message
+    )
+
+    {:noreply, state}
+  end
+
+  def publish(message) do
+    GenServer.cast(__MODULE__, {:publish, message})
+  end
+end
--- mix.exs
+++ mix.exs
@@ -58,11 +58,15 @@ defmodule OpencC2Test.MixProject do
       {:oauth2, "~> 2.0", override: true},
       {:ueberauth, "~> 0.10.5"},
       {:ueberauth_github, "~> 0.8.1"},
-      {:sbom,
-       git: "https://github.com/sigu/sbom.git",
-      #  only: :dev,
-       branch: "auto-install-bom",
-       runtime: false},
+      {
+        :sbom,
+        #  only: :dev,
+        git: "https://github.com/sigu/sbom.git", branch: "auto-install-bom", runtime: false
+      },
+      {:ranch, ">= 0.0.0", manager: :rebar3, override: true},
+      {:emqtt, github: "emqx/emqtt", tag: "1.4.4", system_env: [{"BUILD_WITHOUT_QUIC", "1"}]},
+      {:cowlib, "~> 2.11.0", override: true},
+      {:gun, "~> 1.3.0", override: true}
     ]
   end
 
